958f08abd12035393e18111f024fd8f6e11e86ab
Alexander Courtis <acourtis@atlassian.com>
2015-08-10 19:19:45 +1000

    xmonad: don't use Mirror tiled

--- xmonad.hs
+++ xmonad.hs
@@ -185,7 +185,8 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
 -- The available layouts.  Note that each layout is separated by |||,
 -- which denotes layout choice.
 --
-myLayout = tiled ||| Mirror tiled ||| Full
+--myLayout = tiled ||| Mirror tiled ||| Full
+myLayout = tiled ||| Full
   where
      -- default tiling algorithm partitions the screen into two panes
      tiled   = Tall nmaster delta ratio
