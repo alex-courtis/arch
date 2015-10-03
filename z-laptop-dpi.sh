39af0fee16624b6f99bb8335ad62bf83fe7c983f
Alexander Courtis <acourtis@atlassian.com>
2015-10-03 19:15:42 +1000

    use a default (96) DPI if there is more than just the laptop display connected

--- z-laptop-dpi.sh
+++ z-laptop-dpi.sh
@@ -4,9 +4,13 @@ XRANDR="xrandr"
 XRDB="xrdb"
 LAPTOP_ID="eDP1"
 LAPTOP_DPI="144"
+DEFAULT_DPI="96"
 
+DESIRED_DPI=${DEFAULT_DPI}
 if [ $(${XRANDR} | grep " connected" | wc -l) -eq 1 ]; then
     if [ $(${XRANDR} | grep "${LAPTOP_ID} connected" | wc -l) -eq 1 ]; then
-        echo "Xft.dpi: ${LAPTOP_DPI}" | ${XRDB} -merge
+        DESIRED_DPI=${LAPTOP_DPI}
     fi
 fi
+
+echo "Xft.dpi: ${DESIRED_DPI}" | ${XRDB} -merge
