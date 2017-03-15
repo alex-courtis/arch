e4f89d0e64dbc4a55bbc44eea46e44b16745fae1
Alexander Courtis <acourtis@atlassian.com>
2017-03-16 00:57:23 +1100

    no longer require a system tray, use networkmanager_dmenu, directly launch pavucontrol and refactor xmobarPulseVolume.sh

--- bin/xmobarPulseVolume.sh
+++ bin/xmobarPulseVolume.sh
@@ -1,65 +1,75 @@
 #!/bin/bash
 
 # passes up, down, mute, mute-input to pulseaudio-ctl
-# unmutes instead of up, down, if muted
+# unmutes instead of up or down, if muted
 # outputs status to named pipe ${XDG_RUNTIME_DIR}/xmobarPulseVolume, for display by xmobar
-# right click updates
+# attempts to start ${PA_USER_SERVICE} once, if ${PA_CTL} can't get a reasonable response
 
 PA_CTL="pulseaudio-ctl"
 PA_USER_SERVICE="pulseaudio.service"
-[ ! $(type -t "${PA_CTL}") ] && exit 1
+PA_FAIL_MESSAGE="No PulseAudio daemon running"
 
-# start the user service, if it's not running
-systemctl --user status ${PA_USER_SERVICE} 2>&1 >/dev/null
-if [ ${?} -ne 0 ]; then
-    systemctl --user start ${PA_USER_SERVICE}
-fi
+# only the first argument is noteworthy
+[[ ${#} -eq 1 ]] && ACTION="${1}"
+
+# just give up if we don't have the needful
+[[ ! $(type -t "${PA_CTL}") ]] && exit 1
 
 # setup a named pipe
 PIPE=${XDG_RUNTIME_DIR}/xmobarPulseVolume
-[ -p "${PIPE}" ] || mkfifo "${PIPE}"
+[[ -p "${PIPE}" ]] || mkfifo "${PIPE}"
 
-# only the first argument is noteworthy
-[ ${#} -eq 1 ] && ACTION=${1}
+# retrieve status from pulse audio
+PA_FULL_STATUS=$(${PA_CTL} full-status 2>&1)
 
-# existing status
-CUR_PA_STATUS=($(${PA_CTL} full-status))
-[ "${CUR_PA_STATUS[1]}" == "yes" ] && CUR_OUT_MUTE="true"
+# attempt to start service on fail response - return codes are borken for ${PA_CTL}
+if [[ "${PA_FULL_STATUS}" =~ "${PA_FAIL_MESSAGE}" ]]; then
 
-# unmute if changing volume and we are muted
-if [ "${CUR_OUT_MUTE}" ]; then
-    if [ "${ACTION}" == "up" -o "${ACTION}" == "down" ]; then
-        ${PA_CTL} mute
-        unset ACTION
+    # attempt to start
+    systemctl --user start ${PA_USER_SERVICE}
+
+    # try again but fail hard if the service didn't start
+    PA_FULL_STATUS=$(${PA_CTL} full-status 2>&1)
+    if [[ "${PA_FULL_STATUS}" =~ "${PA_FAIL_MESSAGE}" ]]; then
+        exit 1
     fi
 fi
 
-# enact known actions
+# existing status
+PA_STATUS=(${PA_FULL_STATUS})
+
+# unmute if changing volume and we are muted
+if [[ "${PA_STATUS[1]}" == "yes" && ( "${ACTION}" == "up" || "${ACTION}" == "down" ) ]]; then
+    ACTION="mute"
+fi
+
+# enact only known actions and update status
 case ${ACTION} in
-    "up") ;&
-    "down") ;&
-    "mute") ;&
-    "mute-input")
-        ${PA_CTL} ${ACTION} ;;
+"up") ;&
+"down") ;&
+"mute") ;&
+"mute-input")
+    # enact
+    "${PA_CTL}" "${ACTION}"
+
+    # update status
+    PA_STATUS=($(${PA_CTL} full-status 2>&1))
+;;
 esac
 
-# new status
-PA_STATUS=($(${PA_CTL}  full-status))
-OUT_VOL=${PA_STATUS[0]}
-[ "${PA_STATUS[1]}" == "yes" ] && OUT_MUTE="true"
-[ "${PA_STATUS[2]}" == "yes" ] && IN_MUTE="true"
-
-# output for xmobar
+# format output for xmobar
 OUTPUT="Vo"
-if [ ${OUT_MUTE} ]; then
-    OUTPUT+=" Off"
+if [[ "${PA_STATUS[1]}" == "no" ]]; then
+    # output on at %
+    OUTPUT+=" ${PA_STATUS[0]}%"
 else
-    OUTPUT+=" ${OUT_VOL}%"
+    # output off
+    OUTPUT+=" Off"
 fi
-if [ ! ${IN_MUTE} ]; then
+if [[ "${PA_STATUS[2]}" == "no" ]]; then
+    # input on
     OUTPUT+="   <fc=red>Vi On</fc>"
 fi
-OUTPUT="<action=\`~/bin/xmobarPulseVolume.sh\` button=3>${OUTPUT}</action>"
 
 # write to the pipe
 echo "${OUTPUT}" > ${PIPE}
