f11d41daf51c6d74f4890dc090a1dd5d17d87e65
Alexander Courtis <acourtis@atlassian.com>
2017-03-18 15:39:19 +1100

    add virtual console swap of caps to esc

--- linkSystem.sh
+++ linkSystem.sh
@@ -2,7 +2,7 @@
 
 # links all files in SYSTEM directories to their appropriate place under the filesystem root
 # must be run as root from this directory
-SYSTEM="etc"
+SYSTEM="etc usr"
 ALEX_ROOT=$(pwd)
 
 for f in $(find ${SYSTEM} -type f); do 
