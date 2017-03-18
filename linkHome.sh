948c140795ff05526764770f3e219e03069a33a9
Alexander Courtis <acourtis@atlassian.com>
2017-03-18 19:13:50 +1100

    home link script now creates directories

--- linkHome.sh
+++ linkHome.sh
@@ -2,8 +2,13 @@
 
 # links all files in home directory to their appropriate place under user HOME
 # must be run as user from this directory
-ALEX_HOME=$(pwd)/home
+ALEX_HOME="$(pwd)/home"
 
-for f in $(find ${ALEX_HOME} -type f); do
-    ln -fsv ${f} ${f/${ALEX_HOME}/${HOME}}
+for f in $(find "${ALEX_HOME}" -type f); do
+    TARGET="${f/${ALEX_HOME}/${HOME}}"
+    TARGET_DIR=$(dirname "${TARGET}")
+    if [ ! -d ${TARGET_DIR} ]; then
+        mkdir -pv "${TARGET_DIR}"
+    fi
+    ln -fsv "${f}" "${TARGET}"
 done
