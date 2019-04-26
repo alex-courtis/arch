e7eb155f0646d0e43ee7b81dfd997512a2212f1a
Alexander Courtis <alex@courtis.org>
2019-04-26 18:46:06 +1000

    conditionally copy X gpu specific configurations as they do not play nicely when the hardware is not present

--- link.sh
+++ link.sh
@@ -1,14 +1,40 @@
-#!/bin/bash
+#!/bin/sh
 
 # links all files in home directory to their appropriate place under user HOME
 # must be run as user from this directory
 ALEX_HOME="$(pwd)/home"
 
+lsmod | grep i915 >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+	I915="true"
+else
+	I915="false"
+fi
+
+lsmod | grep amdgpu >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+	AMDGPU="true"
+else
+	AMDGPU="false"
+fi
+
 for f in $(find "${ALEX_HOME}" -type f); do
 	TARGET="${f/${ALEX_HOME}/${HOME}}"
+	TARGET_FILE=$(basename "${TARGET}")
 	TARGET_DIR=$(dirname "${TARGET}")
-	if [ ! -d ${TARGET_DIR} ]; then
-		mkdir -pv "${TARGET_DIR}"
+
+	SKIP="false"
+	if [ "${TARGET_FILE}" = "20-intel.conf" -a "${I915}" = "false" ]; then
+		SKIP="true"
 	fi
-	ln -fsv "${f}" "${TARGET}"
+	if [ "${TARGET_FILE}" = "20-amdgpu.conf" -a "${AMDGPU}" = "false" ]; then
+		SKIP="true"
+	fi
+
+	if [ ${SKIP} = "false" ]; then
+		if [ ! -d ${TARGET_DIR} ]; then
+			mkdir -pv "${TARGET_DIR}"
+		fi
+		ln -fsv "${f}" "${TARGET}"
+	fi
 done
