a58ec837d6f408919ba89b4a64c80e9326a03144
Alexander Courtis <alex@courtis.org>
2022-03-13 14:31:16 +1100

    vim: add nvim-tree logging and lua editorconfig

--- config/nvim/lua/nvt.lua
+++ config/nvim/lua/nvt.lua
@@ -7,53 +7,63 @@ local tree = require'nvim-tree'
 local lib = require'nvim-tree.lib'
 
 local function cd_dot_cb(node)
-	tree.change_dir(vim.fn.getcwd(-1))
-	if node.name ~= ".." then
-		lib.set_index_and_redraw(node.absolute_path)
-	end
+  tree.change_dir(vim.fn.getcwd(-1))
+  if node.name ~= ".." then
+    lib.set_index_and_redraw(node.absolute_path)
+  end
 end
 
 tree.setup {
-	update_cwd = true,
-	update_focused_file = {
-		enable = true,
-	},
-	open_on_setup = true,
-	actions = {
-		open_file = {
-			window_picker = {
-				chars = "aoeui",
-			}
-		}
-	},
-	view = {
-		mappings = {
-			list = {
-				{ key = {"<2-RightMouse>", "<C-]>"},    action = "" }, -- cd
-				{ key = "<C-v>",                        action = "" }, -- vsplit
-				{ key = "<C-x>",                        action = "" }, -- split
-				{ key = "<C-t>",                        action = "" }, -- tabnew
-				{ key = "<BS>",                         action = "" }, -- close_node
-				{ key = "<Tab>",                        action = "" }, -- preview
-				{ key = "D",                            action = "" }, -- trash
-				{ key = "[c",                           action = "" }, -- prev_git_item
-				{ key = "]c",                           action = "" }, -- next_git_item
-				{ key = "-",                            action = "" }, -- dir_up
-				{ key = "s",                            action = "" }, -- system_open
+  log = {
+    enable = true,
+    truncate = true,
+    types = {
+      all = false,
+      config = false,
+      copy_paste = false,
+      git = false,
+    },
+  },
+  update_cwd = true,
+  update_focused_file = {
+    enable = true,
+  },
+  open_on_setup = true,
+  actions = {
+    open_file = {
+      window_picker = {
+        chars = "aoeui",
+      }
+    }
+  },
+  view = {
+    mappings = {
+      list = {
+        { key = {"<2-RightMouse>", "<C-]>"},    action = "" }, -- cd
+        { key = "<C-v>",                        action = "" }, -- vsplit
+        { key = "<C-x>",                        action = "" }, -- split
+        { key = "<C-t>",                        action = "" }, -- tabnew
+        { key = "<BS>",                         action = "" }, -- close_node
+        { key = "<Tab>",                        action = "" }, -- preview
+        { key = "D",                            action = "" }, -- trash
+        { key = "[c",                           action = "" }, -- prev_git_item
+        { key = "]c",                           action = "" }, -- next_git_item
+        { key = "-",                            action = "" }, -- dir_up
+        { key = "s",                            action = "" }, -- system_open
 
-				{ key = "d",                            action = "cd" }, -- remove
-				{ key = "x",                            action = "remove" }, -- cut
+        { key = "d",                            action = "cd" }, -- remove
+        { key = "x",                            action = "remove" }, -- cut
 
-				{ key = "t",                            action = "cut" },
-				{ key = "<Space>k",                     action = "prev_git_item" },
-				{ key = "<Space>j",                     action = "next_git_item" },
-				{ key = "u",                            action = "dir_up" },
-				{ key = "f",                            action = "run_file_command" },
-				{ key = "z",                            action = "close_node" },
+        { key = "t",                            action = "cut" },
+        { key = "<Space>k",                     action = "prev_git_item" },
+        { key = "<Space>j",                     action = "next_git_item" },
+        { key = "u",                            action = "dir_up" },
+        { key = "f",                            action = "run_file_command" },
+        { key = "z",                            action = "close_node" },
 
-				{ key = ".",                            action = "cd_dot",		action_cb = cd_dot_cb, }, -- run_file_command
-			},
-		},
-	},
+        { key = ".",                            action = "cd_dot",		action_cb = cd_dot_cb, }, -- run_file_command
+      },
+    },
+  },
 }
 
