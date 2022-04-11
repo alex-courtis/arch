d972b274e61f3763ac7de8f1305cc845ed04f3ec
Alexander Courtis <alex@courtis.org>
2022-04-11 10:55:59 +1000

    vim: migrate nvim-tree g:

--- config/nvim/lua/amc/nvim-tree.lua
+++ config/nvim/lua/amc/nvim-tree.lua
@@ -2,7 +2,6 @@
 vim.g.nvim_tree_group_empty = 1
 vim.g.nvim_tree_special_files = {}
 vim.g.nvim_tree_show_icons = { git = 1, folders = 0, files = 1, folder_arrows = 0, }
-vim.g.nvim_tree_indent_markers = 1
 
 local tree = require'nvim-tree'
 local lib = require'nvim-tree.lib'
@@ -42,6 +41,11 @@ tree.setup {
   filters = {
     custom = {
       "^.git$",
+    },
+  },
+  renderer = {
+    indent_markers = {
+      enable = true,
     },
   },
   view = {
