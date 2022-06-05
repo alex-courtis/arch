4af69ff160d681bebd8da493aea8403332493330
Alexander Courtis <alex@courtis.org>
2022-06-05 18:08:56 +1000

    surfingkeys: add search engines and tidy

--- config/surfingkeys/sk.js
+++ config/surfingkeys/sk.js
@@ -1,4 +1,4 @@
-settings.focusOnSaved = true
+settings.focusOnSaved = false
 
 api.Hints.setCharacters('aoeuipyqx');
 
@@ -33,15 +33,6 @@ api.map('C', 'gxx');
 api.map('<Ctrl-d>', 'd');
 api.map('<Ctrl-u>', 'u');
 
-// search aliases, changing w from bing to wikip
-api.map('ow', 'oe');
-api.unmap('oe');
-api.map('sw', 'se');
-api.unmap('se');
-api.removeSearchAlias('b');
-api.removeSearchAlias('s');
-api.removeSearchAlias('h');
-
 // open link in new tab
 api.map('F', 'af');
 
@@ -59,12 +50,20 @@ api.map('t', 'on');
 api.unmap('on');
 
 // open bookmarks
-api.mapkey('b', '#8Open a bookmark in current tab', function() { api.Front.openOmnibar(({type: "Bookmarks", tabbed: false})); });
-api.mapkey('B', '#8Open a bookmark in new tab', function() { api.Front.openOmnibar(({type: "Bookmarks", tabbed: true})); });
+api.mapkey('b', '#8Open a bookmark in current tab', function() {
+	api.Front.openOmnibar(({type: "Bookmarks", tabbed: false}));
+});
+api.mapkey('B', '#8Open a bookmark in new tab', function() {
+	api.Front.openOmnibar(({type: "Bookmarks", tabbed: true}));
+});
 
-// open search d with s and S
-api.mapkey('s', '#8Open search with alias d in current tab', function() { api.Front.openOmnibar(({type: "SearchEngine", extra: 'd', tabbed: false})); });
-api.mapkey('S', '#8Open search with alias d in new tab', function() { api.Front.openOmnibar(({type: "SearchEngine", extra: 'd', tabbed: true})); });
+// open search d with s and S; clobbers all the unwanted s prefixes
+api.mapkey('s', '#8Open search with alias du in current tab', function() {
+	api.Front.openOmnibar(({type: "SearchEngine", extra: 'du', tabbed: false}));
+});
+api.mapkey('S', '#8Open search with alias du in new tab', function() {
+	api.Front.openOmnibar(({type: "SearchEngine", extra: 'du', tabbed: true}));
+});
 
 // open url from clipboard
 api.mapkey('p', '#7Open link from clipboard in current tab', function() {
@@ -76,6 +75,35 @@ api.mapkey('P', '#7Open link from clipboard in new tab', function() {
 	api.Clipboard.read(function(response) {
 		api.tabOpenLink(response.data);
 	});
+});
+
+// clear most search aliases
+api.removeSearchAlias('e');
+api.removeSearchAlias('d');
+api.removeSearchAlias('b');
+api.removeSearchAlias('s');
+api.removeSearchAlias('h');
+api.removeSearchAlias('w');
+
+// real search aliases
+// TODO make untabbed, add O prefix for tabbed
+api.addSearchAlias('du', 'duckduckgo', 'https://duckduckgo.com/?q=', 's', 'https://duckduckgo.com/ac/?q=', function(response) {
+	var res = JSON.parse(response.text);
+	return res.map(function(r){
+		return r.phrase;
+	});
+});
+api.addSearchAlias('wi', 'wikipedia', 'https://en.wikipedia.org/wiki/', 's', 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&formatversion=2&namespace=0&limit=40&search=', function(response) {
+	return JSON.parse(response.text)[1];
+});
+api.addSearchAlias('ap', 'Arch Packages', 'https://www.archlinux.org/packages/?q=%s');
+api.addSearchAlias('aw', 'Arch Wiki', 'https://wiki.archlinux.org/index.php?title=Special:Search&search=%s');
+api.addSearchAlias('au', 'AUR Packages', 'https://aur.archlinux.org/packages/?O=0&K=%s');
+api.addSearchAlias('sd', 'Steam DB', 'https://steamdb.info/search/?a=app&q=%s');
+api.addSearchAlias('ss', 'Steam Store', 'https://store.steampowered.com/search/?snr=1_4_4__12&term=%s');
+api.addSearchAlias('dw', 'Dark Souls Wiki', 'https://darksouls.fandom.com/wiki/Special:Search?query=%s&scope=internal&navigationSearch=true');
+api.mapkey('odw', '#8Open Search with alias dw', () => {
+	api.Front.openOmnibar({type: "SearchEngine", extra: 'dw', tabbed: false});
 });
 
 // ace editor
