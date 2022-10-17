d00df5c7bd15d9e315531cb31592cd89ad573389
Alexander Courtis <alex@courtis.org>
2022-10-17 15:40:32 +1100

    vim: migrate to new nvim-cmp-lsp API

--- config/nvim/lua/amc/lsp.lua
+++ config/nvim/lua/amc/lsp.lua
@@ -4,7 +4,7 @@ function M.setup()
   local lspconfig = require("lspconfig")
   local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-  local capabilities = cmp_nvim_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
+  local capabilities = cmp_nvim_lsp.default_capabilities()
 
   lspconfig.ccls.setup({
     capabilities = capabilities,
