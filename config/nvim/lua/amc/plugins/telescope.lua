975ef64a6239b0022bb465aca48f659b0b68a530
Alexander Courtis <alex@courtis.org>
2022-12-12 12:50:56 +1100

    vim: telescope: enter mostly in normal mode, bufexplorer style buffers

--- config/nvim/lua/amc/plugins/telescope.lua
+++ config/nvim/lua/amc/plugins/telescope.lua
@@ -4,6 +4,14 @@ function M.setup()
   require('telescope').setup({
     pickers = {
       buffers = {
+        ignore_current_buffer = true,
+        initial_mode = "normal",
+        sort_mru = true,
+      },
+      grep_string = {
+        initial_mode = "normal",
+      },
+      lsp_references = {
         initial_mode = "normal",
       },
     }
