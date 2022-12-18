3e5b3bf9c5d22d92a9972e99b8e270dab5c4d4e5
Alexander Courtis <alex@courtis.org>
2022-12-18 15:46:25 +1100

    vim: telescope: live_grep: use quickfix and remember last

--- config/nvim/lua/amc/plugins/telescope.lua
+++ config/nvim/lua/amc/plugins/telescope.lua
@@ -1,8 +1,12 @@
 local telescope = require("telescope")
-local history = require("telescope.actions.history")
+local actions = require("telescope.actions")
+local action_set = require("telescope.actions.set")
+local action_state = require("telescope.actions.state")
 local builtin = require("telescope.builtin")
 
-local M = {}
+local M = {
+  last_grep = nil,
+}
 
 local function opts()
   local o = {}
@@ -16,8 +20,42 @@ local function opts()
   return o
 end
 
+local function attach_quickfix_select(prompt_bufnr)
+  actions.select_default:replace(function()
+    local picker = action_state.get_current_picker(prompt_bufnr)
+    local manager = picker.manager
+
+    if manager:num_results() > 0 then
+      -- quickfix handles results
+      local count = picker:get_selection_row() + 1
+
+      -- closes telescope
+      actions.send_to_qflist(prompt_bufnr)
+
+      vim.cmd({ cmd = "cc", count = count })
+      vim.cmd({ cmd = "cwindow", count = 15 })
+
+      return true
+    else
+      -- telescope default
+      return action_set.edit(prompt_bufnr, "default")
+    end
+  end)
+  return true
+end
+
+local function set_last_grep()
+  M.last_grep = action_state.get_current_line()
+end
+
 telescope.setup({
   pickers = {
+    live_grep = {
+      attach_mappings = attach_quickfix_select,
+      on_complete = {
+        set_last_grep,
+      },
+    },
     buffers = {
       ignore_current_buffer = true,
       initial_mode = "normal",
@@ -52,6 +90,9 @@ telescope.setup({
 for n, f in pairs(builtin) do
   if type(f) == "function" then
     M[n] = function(o)
+      if o and o.default_text then
+        M.last_grep = o.default_text
+      end
       return f(vim.tbl_extend("force", opts(), o or {}))
     end
   end
@@ -60,10 +101,7 @@ end
 function M.live_grep_last(o)
   o = vim.tbl_extend("force", opts(), o or {})
 
-  local simple = history.get_simple_history()
-  if simple and type(simple.content) == "table" then
-    o.default_text = simple.content[#simple.content]
-  end
+  o.default_text = M.last_grep
 
   return builtin.live_grep(o)
 end
