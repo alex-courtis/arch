7487bc89a437d886db2ed2ee7d54b4b5b944cc20
Alexander Courtis <alex@courtis.org>
2023-04-16 10:53:15 +1000

    vim: harden keymap: nil rhs

--- config/nvim/lua/amc/keymap.lua
+++ config/nvim/lua/amc/keymap.lua
@@ -17,19 +17,27 @@ LEADERS = {
 
 for _, mode in ipairs(MODES) do
   M[mode .. "m" .. "__"] = function(lhs, rhs)
-    vim.keymap.set(mode, lhs, rhs, { remap = true })
+    if rhs then
+      vim.keymap.set(mode, lhs, rhs, { remap = true })
+    end
   end
   M[mode .. "m" .. "s_"] = function(lhs, rhs)
-    vim.keymap.set(mode, lhs, rhs, { remap = true, silent = true })
+    if rhs then
+      vim.keymap.set(mode, lhs, rhs, { remap = true, silent = true })
+    end
   end
   M[mode .. "m" .. "_l"] = function(lhs, rhs)
-    for _, leader in ipairs(LEADERS) do
-      M[mode .. "m" .. "__"](leader .. lhs, rhs)
+    if rhs then
+      for _, leader in ipairs(LEADERS) do
+        M[mode .. "m" .. "__"](leader .. lhs, rhs)
+      end
     end
   end
   M[mode .. "m" .. "sl"] = function(lhs, rhs)
-    for _, leader in ipairs(LEADERS) do
-      M[mode .. "m" .. "s_"](leader .. lhs, rhs)
+    if rhs then
+      for _, leader in ipairs(LEADERS) do
+        M[mode .. "m" .. "s_"](leader .. lhs, rhs)
+      end
     end
   end
 end
