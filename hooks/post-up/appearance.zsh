6b7e8d0a63fe52c0fac63f6e074708496b8c5c58
Alexander Courtis <alex@courtis.org>
2023-09-30 17:22:08 +1000

    rcm: add appearance substitutions for alacritty, tidy

--- hooks/post-up/appearance.zsh
+++ hooks/post-up/appearance.zsh
@@ -5,14 +5,14 @@
 for f in ${(ps: :)APPEARANCE_FILES}; do
 	src="../../${f}"
 	dst="${HOME}/.${f}"
-	tmp="/tmp/${f}"
+	tmp="/tmp/$(basename ${f})"
 
 	cp "${src}" ${tmp} 
 
 	# all base16 colours
 	for k v in "${(@kv)BASE16}"; do
 		# TODO one sed
-		sed -i -e "s/\$BASE16_${k}/${v}/g" "${tmp}"
+		sed -i -e "s/\$BASE16\[${k}\]/${v}/g" "${tmp}"
 	done
 
 	# font
@@ -21,6 +21,8 @@ for f in ${(ps: :)APPEARANCE_FILES}; do
 	diff "${tmp}" "${dst}" > /dev/null 2>&1
 	if [ $? -ne 0 ]; then
 		mv -v "${tmp}" "${dst}"
+	else
+		rm "${tmp}"
 	fi
 done
 
