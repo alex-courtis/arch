767c73db70e554241e9a2d888833522549cc9835
Alexander Courtis <alex@courtis.org>
2024-01-16 15:12:41 +1100

    vimium: compile from yaml and css

---
+++ config/vimium/compile.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# indent 2 for yaml
+cat vimium-dark-theme.css | sed -e 's/^/  /g' > /tmp/ss.indented.css
+
+# insert the stylesheet and output as json
+cat vimium-options.yaml | sed -e '/STYLESHEET/{r /tmp/ss.indented.css
+d }' | yq -oj > vimium-options.json
