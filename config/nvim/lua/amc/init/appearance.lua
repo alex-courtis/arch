781433d2eb42eba2db5d956f6d311362f89c367c
Alexander Courtis <alex@courtis.org>
2024-01-23 11:08:35 +1100

    vim: nvim-tree appearance following overhaul

--- config/nvim/lua/amc/init/appearance.lua
+++ config/nvim/lua/amc/init/appearance.lua
@@ -17,7 +17,13 @@ vim.cmd("highlight TrailingSpace cterm=undercurl gui=undercurl guisp=#" .. vim.e
 vim.cmd("highlight Search guibg=#" .. vim.env.BASE16_orange) -- default yellow
 vim.cmd("highlight IncSearch guibg=#" .. vim.env.BASE16_yellow) -- default orange
 
--- nvim-tree
-vim.cmd("highlight! link NvimTreeExecFile Italic")
-vim.cmd("highlight! link NvimTreeOpenedFile SpecialChar")
-vim.cmd("highlight! link NvimTreeSymLink Comment")
+-- nvim-tree clear
+vim.cmd("highlight! link NvimTreeImageFile Normal")
+vim.cmd("highlight! link NvimTreeSpecialFile Normal")
+vim.cmd("highlight! link NvimTreeExecFile Normal")
+
+-- nvim-tree override
+vim.cmd("highlight! link NvimTreeRootFolder Identifier")
+vim.cmd("highlight! link NvimTreeSymLink Italic")
+vim.cmd("highlight! link NvimTreeModifiedIcon NvimTreeOpenedHL")
+
