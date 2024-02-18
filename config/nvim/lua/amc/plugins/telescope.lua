5f065dcca1accd1277aaae063f5878b979c1e946
Alexander Courtis <alex@courtis.org>
2024-02-18 17:37:58 +1100

    Revert "vim: telescope: use builtin grep_string"
    This reverts commit e52456df295f756178307dbfaed29e599b5a37c6.

--- config/nvim/lua/amc/plugins/telescope.lua
+++ config/nvim/lua/amc/plugins/telescope.lua
@@ -49,10 +49,6 @@ local cfg = actions
         live_grep = {
           attach_mappings = attach_quickfix_select,
         },
-        grep_string = {
-          initial_mode = "normal",
-          attach_mappings = attach_quickfix_select,
-        },
         git_status = {
           initial_mode = "normal",
           path_display = {
