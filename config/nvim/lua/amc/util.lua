bf47057d237337f70842a97e675f07ad104690b3
Alexander Courtis <alex@courtis.org>
2024-03-16 11:23:33 +1100

    vim: use ~/.nvt-dir

--- config/nvim/lua/amc/util.lua
+++ config/nvim/lua/amc/util.lua
@@ -3,7 +3,7 @@ local M = {}
 ---Read .nvt-dir and return first path containing /lua/nvim-tree.lua
 ---@return string dir from file otherwise "nvim-tree/nvim-tree.lua"
 function M.nvt_plugin_dir()
-  local config_file_path = vim.fn.stdpath("config") .. "/.nvt-dir"
+  local config_file_path = vim.env.HOME .. "/.nvt-dir"
   if vim.loop.fs_stat(config_file_path) then
     for nvt_dir in io.lines(config_file_path) do
       if vim.loop.fs_stat(nvt_dir .. "/lua/nvim-tree.lua") then
