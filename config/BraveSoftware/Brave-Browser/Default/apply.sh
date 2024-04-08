e4936898fa74e0b7a168532a828bb10c707bb80f
Alexander Courtis <alex@courtis.org>
2024-04-08 11:29:10 +1000

    brave: persist keys

--- config/BraveSoftware/Brave-Browser/Default/apply.sh
+++ config/BraveSoftware/Brave-Browser/Default/apply.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
+cp "${HOME}/.config/BraveSoftware/Brave-Browser/Default/Preferences" "${HOME}/.config/BraveSoftware/Brave-Browser/Default/Preferences.$(date +%Y%m%d_%H%M%S)"
+
 jq --slurp --compact-output \
 	'.[0].brave.accelerators=.[1] |
 	.[0].brave.default_accelerators=.[2] |
 	.[0].extensions.commands=.[3] |
 	.[0]' \
-	~/.config/BraveSoftware/Brave-Browser/Default/Preferences \
+	"${HOME}/.config/BraveSoftware/Brave-Browser/Default/Preferences" \
 	brave.accelerators.json \
 	brave.default_accelerators.json \
 	extensions.commands.json \
-	> /home/alex/.config/BraveSoftware/Brave-Browser/Default/Preferences.ins.json
+	> "${HOME}/.config/BraveSoftware/Brave-Browser/Default/Preferences.json"
+
+mv "${HOME}/.config/BraveSoftware/Brave-Browser/Default/Preferences.json" "${HOME}/.config/BraveSoftware/Brave-Browser/Default/Preferences"
