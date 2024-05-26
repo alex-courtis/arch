b685da5fd86f0c9a739efec597af1db9dde0a048
Alexander Courtis <alex@courtis.org>
2024-05-26 10:46:57 +1000

    vim: treesitter: move parsers to site, list desired

--- config/nvim/lua/amc/plugins/treesitter.lua
+++ config/nvim/lua/amc/plugins/treesitter.lua
@@ -4,17 +4,62 @@ if not configs_ok then
   return
 end
 
+local parsers = {
+  "awk",
+  "bash",
+  "cmake",
+  "cpp",
+  "c",
+  "diff",
+  "dockerfile",
+  "gitattributes",
+  "gitcommit",
+  "git_config",
+  "gitignore",
+  "git_rebase",
+  "go",
+  "graphql",
+  "html",
+  "ini",
+  "jq",
+  "json5",
+  "jsonc",
+  "json",
+  "luadoc",
+  "luap",
+  "luau",
+  "make",
+  "meson",
+  "ninja",
+  "passwd",
+  "perl",
+  "printf",
+  "properties",
+  "proto",
+  "rust",
+  "sql",
+  "ssh_config",
+  "terraform",
+  "tmux",
+  "todotxt",
+  "toml",
+  "typescript",
+  "typespec",
+  "yaml",
+}
+
 -- most of these do not look good
 -- install explicitly e.g. :TSInstall cpp
 
 -- :TSBufToggle highlight
 
-configs.setup({
-  modules = {},
+local opts = {
+  parser_install_dir = vim.fn.stdpath("data"),
 
   sync_install = false,
 
-  ensure_installed = {},
+  -- TODO find a way to ensure parsers are not checked on every startup OR just auto install and disable
+  -- ensure_installed = parsers,
 
   ignore_install = {},
 
@@ -25,4 +70,6 @@ configs.setup({
 
     disable = {},
   },
-})
+}
+
+configs.setup(opts)
