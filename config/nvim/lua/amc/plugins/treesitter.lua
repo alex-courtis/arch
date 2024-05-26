4b6ff8c8f68d390af12175a01f8d8c9d2cfedaff
Alexander Courtis <alex@courtis.org>
2024-05-26 11:02:57 +1000

    vim: treesitter: move parsers to site, desired are a one-time install

--- config/nvim/lua/amc/plugins/treesitter.lua
+++ config/nvim/lua/amc/plugins/treesitter.lua
@@ -4,62 +4,15 @@ if not configs_ok then
   return
 end
 
-local parsers = {
-  "awk",
-  "bash",
-  "cmake",
-  "cpp",
-  "c",
-  "diff",
-  "dockerfile",
-  "gitattributes",
-  "gitcommit",
-  "git_config",
-  "gitignore",
-  "git_rebase",
-  "go",
-  "graphql",
-  "html",
-  "ini",
-  "jq",
-  "json5",
-  "jsonc",
-  "json",
-  "luadoc",
-  "luap",
-  "luau",
-  "make",
-  "meson",
-  "ninja",
-  "passwd",
-  "perl",
-  "printf",
-  "properties",
-  "proto",
-  "rust",
-  "sql",
-  "ssh_config",
-  "terraform",
-  "tmux",
-  "todotxt",
-  "toml",
-  "typescript",
-  "typespec",
-  "yaml",
-}
-
 -- most of these do not look good
 -- install explicitly e.g. :TSInstall cpp
 
 -- :TSBufToggle highlight
 
 local opts = {
-  parser_install_dir = vim.fn.stdpath("data"),
+  parser_install_dir = vim.fn.stdpath("data") .. "/treesitter",
 
   sync_install = false,
-
-  -- TODO find a way to ensure parsers are not checked on every startup OR just auto install and disable
-  -- ensure_installed = parsers,
 
   ignore_install = {},
 
@@ -72,4 +25,53 @@ local opts = {
   },
 }
 
+-- this checks every startup; just run it once
+if false then
+  opts.ensure_installed = {
+    "awk",
+    "bash",
+    "cmake",
+    "cpp",
+    "c",
+    "diff",
+    "dockerfile",
+    "gitattributes",
+    "gitcommit",
+    "git_config",
+    "gitignore",
+    "git_rebase",
+    "go",
+    "graphql",
+    "html",
+    "ini",
+    "jq",
+    "json5",
+    "jsonc",
+    "json",
+    "luadoc",
+    "luap",
+    "luau",
+    "make",
+    "meson",
+    "ninja",
+    "passwd",
+    "perl",
+    "printf",
+    "properties",
+    "proto",
+    "rust",
+    "sql",
+    "ssh_config",
+    "terraform",
+    "tmux",
+    "todotxt",
+    "toml",
+    "typescript",
+    "typespec",
+    "yaml",
+  }
+end
+
 configs.setup(opts)
+
+vim.opt.runtimepath:append(opts.parser_install_dir)
