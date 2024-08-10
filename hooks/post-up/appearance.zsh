a19fb1619bad745bf1040ea0db2963df998495cf
Alexander Courtis <alex@courtis.org>
2024-08-10 10:52:44 +1000

    explicit FONT_SIZE_PT_BAR

--- hooks/post-up/appearance.zsh
+++ hooks/post-up/appearance.zsh
@@ -22,7 +22,7 @@ for f in ${(ps: :)APPEARANCE_FILES}; do
 	sed -i -e "${base16sed}" "${tmp}"
 
 	# font
-	sed -i -e "s/\$FONT_SIZE_PT_EVEN/${FONT_SIZE_PT_EVEN}/g" "${tmp}"
+	sed -i -e "s/\$FONT_SIZE_PT_BAR/${FONT_SIZE_PT_BAR}/g" "${tmp}"
 	sed -i -e "s/\$FONT_SIZE_PT/${FONT_SIZE_PT}/g" "${tmp}"
 
 	# highlight colour
