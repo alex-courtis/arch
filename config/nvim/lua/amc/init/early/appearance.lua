55267db39c12c8a27226efdc49354cc949f1b8aa
Alexander Courtis <alex@courtis.org>
2024-09-01 11:35:47 +1000

    vim: linux-16color only 8 colour background fixes

--- config/nvim/lua/amc/init/early/appearance.lua
+++ config/nvim/lua/amc/init/early/appearance.lua
@@ -13,10 +13,6 @@ vim.cmd("highlight! link NvimTreeRootFolder NONE")
 vim.cmd("highlight! link NvimTreeNormal Comment")
 vim.cmd("highlight! link NvimTreeSymLink Italic")
 
-if not vim.env.TERM:match("^linux") then
-  vim.o.cursorline = true
-end
-
 vim.cmd("colorscheme base16")
 
 vim.cmd("highlight Search guibg=#" .. vim.env.BASE16_orange) -- default yellow
@@ -25,8 +21,31 @@ vim.cmd("highlight IncSearch guibg=#" .. vim.env.BASE16_yellow) -- default orang
 -- nvim-tree override
 vim.cmd("highlight NvimTreeOpenedHL guifg=#" .. vim.env.BASE16_default_foreground)
 
--- linux terminal hacks awaiting a proper 8/16 colour cterm palette for base16
+-- linux-16color shows 16 foreground colours but only 8 background colours
 if vim.env.TERM:match("^linux") then
-  vim.cmd("highlight Visual ctermfg=0 ctermbg=15")
-end
+  -- change the low grey backgrounds to white
+  local hl = vim.api.nvim_get_hl(0, {})
+  for name, h in pairs(hl) do
+    if type(h.ctermbg) == "number" and tonumber(h.ctermbg) >= 236 and tonumber(h.ctermbg) <= 243 then
+      h.ctermbg = 244
+      vim.api.nvim_set_hl(0, name, h)
+    end
+  end
 
+  -- ensure white background are visible
+  vim.cmd("highlight CursorLine ctermfg=0")
+  vim.cmd("highlight CursorLineNr ctermfg=15")
+  vim.cmd("highlight CursorLineSign ctermfg=15")
+  vim.cmd("highlight Folded ctermfg=0")
+  vim.cmd("highlight LineNr ctermfg=0")
+  vim.cmd("highlight LineNrAbove ctermfg=0")
+  vim.cmd("highlight LineNrBelow ctermfg=0")
+  vim.cmd("highlight SignColumn ctermfg=0")
+  vim.cmd("highlight StatusLine ctermfg=0")
+  vim.cmd("highlight StatusLineNC ctermfg=0")
+  vim.cmd("highlight TabLine ctermfg=0")
+  vim.cmd("highlight TabLineFill ctermfg=0")
+  vim.cmd("highlight Visual ctermfg=0")
+  vim.cmd("highlight WinBar ctermfg=0")
+  vim.cmd("highlight WinBarNC ctermfg=0")
+end
