fbe04088eafc88ea37bcb716990ae576f4931b1f
Alexander Courtis <alex@courtis.org>
2024-09-30 10:45:54 +1000

    stylua -> EmmyLuaCodeStyle: map.lua

--- config/nvim/lua/amc/init/late/map.lua
+++ config/nvim/lua/amc/init/late/map.lua
@@ -22,21 +22,6 @@ for _, mode in ipairs({ "n", "i", "c", "v", "x", "s", "o" }) do
 end
 -- stylua: ignore end
 
--- hacky vim clipboard=autoselect https://github.com/neovim/neovim/issues/2325
-K.vm__("<LeftRelease>", '"*ygv')
-
-K.nm__(";", ":")
-K.vm__(";", ":")
-K.nm__("q;", "q:")
-K.vm__("q;", "q:")
-
-K.nms_("yn", ':let @+ = expand("%:t")<CR>')
-K.nms_("yr", ':let @+ = expand("%:.")<CR>')
-K.nms_("ya", ':let @+ = expand("%:p")<CR>')
-
-K.cm__("<C-j>", "<Down>")
-K.cm__("<C-k>", "<Up>")
-
 -- normal mode escape clears highlight
 local ESC = vim.api.nvim_replace_termcodes("<ESC>", true, false, true)
 K.nm__("<Esc>", function()
@@ -44,128 +29,123 @@ K.nm__("<Esc>", function()
   vim.api.nvim_feedkeys(ESC, "n", false)
 end)
 
---
--- begin left
---
+-- hacky vim clipboard=autoselect https://github.com/neovim/neovim/issues/2325
+K.vm__("<LeftRelease>",  '"*ygv')
 
--- &
--- $
--- @
--- fn
+K.nms_("<Space><BS>",    ":silent BB<CR>")
+K.nms_("<BS><BS>",       ":silent BB<CR>")
 
--- [
-K.nmsl(";", vim.cmd.copen)
-K.nmsl(":", vim.cmd.cclose)
-K.nmsl("a", nvt.open_find)
-K.nmsl("A", nvt.open_find_update_root)
-K.nmsl("'", windows.close_inc)
-K.nmsl('"', windows.close_others)
-
--- {
-K.nmsl(",", fugitive.open)
-K.nmsl("o", windows.go_home_or_next)
-K.nmsl("O", vim.cmd.only)
-K.nmsl("q", windows.close)
-
-K.nm_l("}", ": <C-r>=expand('%:p')<CR><Home>")
-K.nm_l("3", ": <C-r>=expand('%:.')<CR><Home>")
-K.nmsl(".", lsp.goto_next)
-K.nmsl("e", windows.cnext)
--- j gitsigns.next_hunk
-
-K.nm_l("(", ": <C-r>=expand('<cword>')<CR><Home>")
-K.vm_l("(", '"*y: <C-r>=getreg("*")<CR><Home>')
-K.nmsl("p", lsp.goto_prev)
-K.nmsl("u", windows.cprev)
--- k gitsigns.prev_hunk
-
--- =
-K.nmsl("y", telescope.git_status)
-K.nmsl("i", telescope.buffers)
-K.nm_l("I", telescope.builtin)
-K.nmsl("x", ":silent BA<CR>")
-
-K.nms_("<Space><BS>", ":silent BB<CR>")
-K.nms_("<BS><BS>", ":silent BB<CR>")
+K.nms_("<BS><Space>",    ":silent BF<CR>")
+K.nms_("<Space><Space>", ":silent BF<CR>")
 
 --
--- end left
+-- left
 --
+K.nm__(";",     ":")
+K.vm__(";",     ":")
+K.nm__("q;",    "q:")
+K.vm__("q;",    "q:")
+
+K.nms_("yn",    ':let @+ = expand("%:t")<CR>')
+K.nms_("yr",    ':let @+ = expand("%:.")<CR>')
+K.nms_("ya",    ':let @+ = expand("%:p")<CR>')
+
+K.cm__("<C-j>", "<Down>")
+K.cm__("<C-k>", "<Up>")
+
+K.nmsl(";",     vim.cmd.copen)
+K.nmsl(":",     vim.cmd.cclose)
+K.nmsl("a",     nvt.open_find)
+K.nmsl("A",     nvt.open_find_update_root)
+K.nmsl("'",     windows.close_inc)
+K.nmsl('"',     windows.close_others)
+
+K.nmsl(",",     fugitive.open)
+K.nmsl("o",     windows.go_home_or_next)
+K.nmsl("O",     vim.cmd.only)
+K.nmsl("q",     windows.close)
+
+K.nm_l("}",     ": <C-r>=expand('%:p')<CR><Home>")
+K.nm_l("3",     ": <C-r>=expand('%:.')<CR><Home>")
+K.nmsl(".",     lsp.goto_next)
+K.nmsl("e",     windows.cnext)
+
+K.nm_l("(",     ": <C-r>=expand('<cword>')<CR><Home>")
+K.vm_l("(",     '"*y: <C-r>=getreg("*")<CR><Home>')
+K.nmsl("p",     lsp.goto_prev)
+K.nmsl("u",     windows.cprev)
+
+K.nmsl("y",     telescope.git_status)
+K.nmsl("i",     telescope.buffers)
+K.nm_l("I",     telescope.builtin)
+K.nmsl("x",     ":silent BA<CR>")
 
 --
--- begin right
+-- right
 --
 
-K.nm__("*", "<Plug>(asterisk-z*)")
-K.nm_l("*", "*")
-K.nmsl("f", telescope.find_files)
-K.nmsl("F", telescope.find_files_hidden)
+K.nm__("*",  "<Plug>(asterisk-z*)")
+K.nm_l("*",  "*")
+K.nmsl("f",  telescope.find_files)
+K.nmsl("F",  telescope.find_files_hidden)
 K.nmsl("da", vim.lsp.buf.code_action)
 K.nmsl("dq", vim.diagnostic.setqflist)
 K.nmsl("df", vim.diagnostic.open_float)
 K.nmsl("dh", vim.lsp.buf.hover)
 K.nmsl("dl", telescope.diagnostics)
 K.nmsl("dr", dev.lsp_rename)
-K.nmsl("b", ":%y<CR>")
-K.nmsl("B", ":%d_<CR>")
+K.nmsl("b",  ":%y<CR>")
+K.nmsl("B",  ":%d_<CR>")
 
--- )
-K.nmsl("g", telescope.live_grep)
-K.nmsl("G", telescope.live_grep_hidden)
+K.nmsl("g",  telescope.live_grep)
+K.nmsl("G",  telescope.live_grep_hidden)
 K.nmsl("hb", ":G blame<CR>")
--- h* gitsigns
 K.nmsl("mc", dev.clean)
 K.nmsl("mi", dev.install)
 K.nmsl("mm", dev.build)
 K.nmsl("mt", dev.test)
 K.nmsl("ms", dev.source)
 
-K.nmsl("+", rainbow.toggle)
+K.nmsl("+",  rainbow.toggle)
 K.nmsl("cu", "<Plug>Commentary<Plug>Commentary")
 K.nmsl("cc", "<Plug>CommentaryLine")
-K.omsl("c", "<Plug>Commentary")
-K.nmsl("c", "<Plug>Commentary")
-K.xmsl("c", "<Plug>Commentary")
-K.nmsl("t", lsp.goto_definition_or_tag)
-K.nmsl("T", vim.lsp.buf.declaration)
-K.nmsl("w", "<Plug>ReplaceWithRegisterOperatoriw")
-K.xmsl("w", "<Plug>ReplaceWithRegisterVisual")
-K.nmsl("W", "<Plug>ReplaceWithRegisterLine")
+K.omsl("c",  "<Plug>Commentary")
+K.nmsl("c",  "<Plug>Commentary")
+K.xmsl("c",  "<Plug>Commentary")
+K.nmsl("t",  lsp.goto_definition_or_tag)
+K.nmsl("T",  vim.lsp.buf.declaration)
+K.nmsl("w",  "<Plug>ReplaceWithRegisterOperatoriw")
+K.xmsl("w",  "<Plug>ReplaceWithRegisterVisual")
+K.nmsl("W",  "<Plug>ReplaceWithRegisterLine")
 
--- ]
-K.nm_l("r", ":%s/<C-r>=expand('<cword>')<CR>/")
-K.nm_l("R", ":%s/<C-r>=expand('<cword>')<CR>/<C-r>=expand('<cword>')<CR>")
-K.vm_l("r", '"*y:%s/<C-r>=getreg("*")<CR>/')
-K.vm_l("R", '"*y:%s/<C-r>=getreg("*")<CR>/<C-r>=getreg("*")<CR>')
-K.nmsl("n", telescope.lsp_references)
-K.nmsl("N", vim.diagnostic.setqflist)
-K.nmsl("v", ":put<CR>'[v']=")
-K.nmsl("V", ":put!<CR>'[v']=")
+K.nm_l("r",  ":%s/<C-r>=expand('<cword>')<CR>/")
+K.nm_l("R",  ":%s/<C-r>=expand('<cword>')<CR>/<C-r>=expand('<cword>')<CR>")
+K.vm_l("r",  '"*y:%s/<C-r>=getreg("*")<CR>/')
+K.vm_l("R",  '"*y:%s/<C-r>=getreg("*")<CR>/<C-r>=getreg("*")<CR>')
+K.nmsl("n",  telescope.lsp_references)
+K.nmsl("N",  vim.diagnostic.setqflist)
+K.nmsl("v",  ":put<CR>'[v']=")
+K.nmsl("V",  ":put!<CR>'[v']=")
 
--- !
-K.nmsl("l", buffers.toggle_list)
-K.nmsl("L", buffers.trim_whitespace)
-K.nm_l("s", ":lua require('amc.plugins.telescope').live_grep( { default_text = '<C-r>=expand('<cword>')<CR>', initial_mode = \"normal\" })<CR>")
-K.nm_l("S", ":lua require('amc.plugins.telescope').live_grep( { default_text = '<C-r>=expand('<cWORD>')<CR>', initial_mode = \"normal\" })<CR>")
-K.vmsl("s", "\"*y<Esc>:<C-u>lua require('amc.plugins.telescope').live_grep( { default_text = '<C-r>=getreg(\"*\")<CR>' })<CR>")
-K.nmsl("z", dev.format)
+K.nmsl("l",  buffers.toggle_list)
+K.nmsl("L",  buffers.trim_whitespace)
+K.nm_l("s",
+  ":lua require('amc.plugins.telescope').live_grep( { default_text = '<C-r>=expand('<cword>')<CR>', initial_mode = \"normal\" })<CR>")
+K.nm_l("S",
+  ":lua require('amc.plugins.telescope').live_grep( { default_text = '<C-r>=expand('<cWORD>')<CR>', initial_mode = \"normal\" })<CR>")
+K.vmsl("s",  "\"*y<Esc>:<C-u>lua require('amc.plugins.telescope').live_grep( { default_text = '<C-r>=getreg(\"*\")<CR>' })<CR>")
+K.nmsl("z",  dev.format)
 
-K.nm__("#", "<Plug>(asterisk-z#)")
-K.nm_l("#", "#")
--- /
-K.nm_l("-", ":GotoHeaderSwitch<CR>")
-K.nm_l("_", ":GotoHeader<CR>")
+K.nm__("#",  "<Plug>(asterisk-z#)")
+K.nm_l("#",  "#")
+K.nm_l("-",  ":GotoHeaderSwitch<CR>")
+K.nm_l("_",  ":GotoHeader<CR>")
 K.nmsl("\\", ":silent BW!<CR>")
-K.nmsl("|", buffers.wipe_all)
-
-K.nms_("<BS><Space>", ":silent BF<CR>")
-K.nms_("<Space><Space>", ":silent BF<CR>")
+K.nmsl("|",  buffers.wipe_all)
 
 --
--- end right
---
-
 -- vim-vsnip - <Plug> links to the wrong place
+--
 vim.cmd([[
 imap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)' : '<Tab>'
 smap <expr> <Tab>   vsnip#jumpable(1)   ? '<Plug>(vsnip-jump-next)' : '<Tab>'
@@ -176,11 +156,12 @@ smap <expr> <S-Tab> vsnip#jumpable(-1)  ? '<Plug>(vsnip-jump-prev)' : '<S-Tab>'
 --
 -- functions
 --
+
 --- au VimEnter
 function M.clear_default_mappings()
   -- commentary defaults
-  pcall(vim.keymap.del,{"n", "o", "v"}, "gc")
-  pcall(vim.keymap.del,"n", "gcc")
+  pcall(vim.keymap.del, { "n", "o", "v" }, "gc")
+  pcall(vim.keymap.del, "n",               "gcc")
 end
 
 --- au BufEnter
