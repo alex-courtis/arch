a023a53400d92f3f3e478a45e19966d1fcae0428
Alexander Courtis <alex@courtis.org>
2025-03-31 09:30:24 +1100

    vim: keep .luarc.json and .editorconfig in sync

--- config/nvim/lua/amc/init/late/map.lua
+++ config/nvim/lua/amc/init/late/map.lua
@@ -172,16 +172,16 @@ K.nmsl("mT", dev.test_all)
 K.nmsl("ms", dev.source)
 
 -- 4cC
-K.nmsl("+", rainbow.toggle)
+K.nmsl("+",  rainbow.toggle)
 K.nmsl("cu", nop)
 K.nmsl("cc", nop)
 K.omsl("c",  nop)
 K.nmsl("c",  nop)
 K.xmsl("c",  nop)
-K.nmsl("t", "<C-]>")
-K.nmsl("w", "<Plug>ReplaceWithRegisterOperatoriw")
-K.xmsl("w", "<Plug>ReplaceWithRegisterVisual")
-K.nmsl("W", "<Plug>ReplaceWithRegisterLine")
+K.nmsl("t",  "<C-]>")
+K.nmsl("w",  "<Plug>ReplaceWithRegisterOperatoriw")
+K.xmsl("w",  "<Plug>ReplaceWithRegisterVisual")
+K.nmsl("W",  "<Plug>ReplaceWithRegisterLine")
 
 -- ]6N
 K.nm_l("r", ":%s/<C-r>=expand('<cword>')<CR>/<C-r>=expand('<cword>')<CR>")
