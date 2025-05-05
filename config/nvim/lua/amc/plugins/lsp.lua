8d72be76d480c5e88dfd9d1c1586b6bf500fd6b2
Alexander Courtis <alex@courtis.org>
2025-05-05 11:28:15 +1000

    vim: use c-] instead of lsp.buf.definition as it doesn't return any ambiguities

--- config/nvim/lua/amc/plugins/lsp.lua
+++ config/nvim/lua/amc/plugins/lsp.lua
@@ -55,13 +55,20 @@ end
 --- @param bufnr number
 --- @param client vim.lsp.Client
 local function on_attach(client, bufnr)
+
+  -- see :help lsp-defaults
   pcall(vim.keymap.del, "n", "K", { buffer = bufnr })
 
   for _, leader in ipairs({ "<space>", "<bs>" }) do
+
+    -- no need to rebind c-]
+    -- vim.lsp.tagfunc() indicates that it calls textDocument/definition,
+    -- however vim.lsp.buf.definition returns two results e.g. wk.Spec
     if client.server_capabilities.definitionProvider then
-      vim.keymap.set("n", leader .. "t",  vim.lsp.buf.definition, { buffer = bufnr, desc = "vim.lsp.buf.definition", })
+      -- vim.keymap.set("n", leader .. "t",  vim.lsp.buf.definition, { buffer = bufnr, desc = "vim.lsp.buf.definition", })
       vim.keymap.set("n", leader .. "dt", vim.lsp.buf.definition, { buffer = bufnr, desc = "vim.lsp.buf.definition", })
     end
+
     if client.server_capabilities.declarationProvider then
       vim.keymap.set("n", leader .. "T",  vim.lsp.buf.declaration, { buffer = bufnr, desc = "vim.lsp.buf.declaration", })
       vim.keymap.set("n", leader .. "dT", vim.lsp.buf.declaration, { buffer = bufnr, desc = "vim.lsp.buf.declaration", })
@@ -78,11 +85,6 @@ local function on_attach(client, bufnr)
     vim.keymap.set("n", leader .. "dl", telescope.diagnostics,     { buffer = bufnr, desc = "telescope.diagnostics", })
     vim.keymap.set("n", leader .. "dq", vim.diagnostic.setqflist,  { buffer = bufnr, desc = "vim.diagnostic.setqflist", })
   end
-
-  -- TODO try vim.lsp.buf.type_definition()
-
-  -- TODO why is c-] vim.lsp.tagfunc() different to vim.lsp.buf.definition() ? It sometimes returns less results.
-  -- navigate to wk.Spec and the latter returns two
 end
 
 --
