aea22248b25fb5d0b8f2e78a216ada7ce8aa854c
Alexander Courtis <alex@courtis.org>
2025-05-17 15:21:14 +1000

    qutebrowser: bitwarden and some binds

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -1,14 +1,15 @@
 config.load_autoconfig(False)
 
-c.qt.force_platform = 'wayland'
-
 # not optimal; maybe fixed ~20250517 https://github.com/qutebrowser/qutebrowser/issues/8535
-c.qt.force_software_rendering = 'chromium'
+# only appears necessary when xwayland not present
+# c.qt.force_software_rendering = 'chromium'
 
 c.colors.webpage.darkmode.enabled = True
 
 c.url.start_pages  = ['about:blank']
 c.url.default_page = 'about:blank'
+
+c.tabs.position = 'top'
 
 config.unbind('q')
 config.unbind('D')
@@ -27,4 +28,6 @@ config.bind('d', 'scroll-page 0 0.5')
 config.bind('u', 'scroll-page 0 -0.5')
 
 config.bind('<ctrl+alt+t>', 'undo -w')
+
+config.bind('<alt+l>', 'spawn --userscript qute-bitwarden')
 
