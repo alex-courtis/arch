437bb30979fabcd52e959565244293d3a35cc919
Alexander Courtis <alex@courtis.org>
2025-05-23 15:03:21 +1000

    qutebrowser: add quickmarks, bookmarks, search engines

--- config/qutebrowser/config.py
+++ config/qutebrowser/config.py
@@ -16,7 +16,7 @@ c.colors.webpage.preferred_color_scheme = 'dark'
 
 c.fonts.default_size = '13.5pt'
 
-c.editor.command = ["alacritty", "-e", "nvim", "-f", "{file}", "-c", "normal {line}G{column0}l"]
+c.editor.command = [ 'alacritty', '-e', 'nvim', '-f', '{file}', '-c', 'normal {line}G{column0}l' ]
 
 c.hints.chars = 'pgcrlaoeuhtnsqjkmwvz'
 
@@ -28,16 +28,32 @@ c.tabs.show = 'multiple'
 c.tabs.title.format = '{audio}{private}{current_title}'
 c.tabs.title.format_pinned = '{audio}{private}{current_title}'
 
-c.tabs.padding = {"bottom": 3, "left": 0, "right": 0, "top": 2}
-c.tabs.indicator.padding = {"bottom": 0, "left": 3, "right": 10, "top": 0}
+c.tabs.padding = {'bottom': 3, 'left': 0, 'right': 0, 'top': 2}
+c.tabs.indicator.padding = {'bottom': 0, 'left': 3, 'right': 10, 'top': 0}
 c.tabs.indicator.width = 5
 
 c.tabs.title.alignment = 'center'
 
 c.statusbar.position = 'top'
-c.statusbar.padding = {"bottom": 3, "left": 0, "right": 8, "top": 2}
+c.statusbar.padding = {'bottom': 3, 'left': 0, 'right': 8, 'top': 2}
 
-c.statusbar.widgets = ["keypress", "search_match", "history", "progress", "url"]
+c.statusbar.widgets = ['keypress', 'search_match', 'history', 'progress', 'url']
+
+c.url.searchengines = {
+		'DEFAULT': 'https://duckduckgo.com/?q={}',
+		'ap':      'https://archlinux.org/packages/?q={}',
+		'au':      'https://aur.archlinux.org/packages?K={}',
+		'aw':      'https://wiki.archlinux.org/index.php?search={}',
+		'd':       'https://duckduckgo.com/?q={}',
+		'di':      'https://duckduckgo.com/?iar=images&q={}',
+		'g':       'https://google.com/search?q={}',
+		'gi':      'https://images.google.com/search?q={}',
+		'pd':      'https://www.protondb.com/search?q={}',
+		'sd':      'https://steamdb.info/instantsearch/?query={}',
+		'ss':      'https://store.steampowered.com/search/?term={}',
+		'w':       'https://www.wikipedia.org/w/index.php?title=Special:Search&search={}',
+		'y':       'https://www.youtube.com/results?search_query={}',
+		}
 
 config.unbind('q')
 config.unbind('D')
--- zshenv
+++ zshenv
@@ -31,6 +31,7 @@ export XDG_CACHE_HOME=$HOME/.cache
 export XDG_CONFIG_HOME=$HOME/.config
 export XDG_DATA_HOME=$HOME/.local/share
 export XDG_STATE_HOME=$HOME/.local/state
+export XDG_MUSIC_DIR=$HOME/.music
 
 # don't minimise fullscreen SDL (i.e. most steam) when losing focus
 # they send a _NET_WM_STATE_FULLSCREEN _NET_WM_STATE_REMOVE however never send an ADD message on regaining focus
