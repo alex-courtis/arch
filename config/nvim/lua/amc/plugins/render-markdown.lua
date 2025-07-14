5215c326ea80035396b4311d4ffdbe50033c57c6
Alexander Courtis <alex@courtis.org>
2025-07-14 13:37:48 +1000

    vim: render-markdown minimal config

--- config/nvim/lua/amc/plugins/render-markdown.lua
+++ config/nvim/lua/amc/plugins/render-markdown.lua
@@ -1,5 +1,21 @@
-require("render-markdown").setup({
+local require = require("amc.require").or_nil
+
+local render_markdown = require("render-markdown")
+
+if not render_markdown then
+  return
+end
+
+---@type render.md.UserConfig
+local config = {
+  render_modes = true,
   checkbox = {
     enabled = true,
-  }
-})
+  },
+  code = {
+    enabled = true,
+    border = "thin",
+  },
+}
+
+render_markdown.setup(config);
