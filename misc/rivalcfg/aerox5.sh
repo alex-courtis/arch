c38520ce02d5edd49ce0636b6cfaa5f2496de446
Alexander Courtis <alex@courtis.org>
2025-09-28 10:02:50 +1000

    rival: aerox5 dpi

--- misc/rivalcfg/aerox5.sh
+++ misc/rivalcfg/aerox5.sh
@@ -6,6 +6,6 @@ rivalcfg --default-lighting off
 
 rivalcfg --dim-timer 1
 
-rivalcfg --sensitivity "250,500,750"
+rivalcfg --sensitivity "250,500,750,1000,1250"
 
 rivalcfg -b "buttons(button1=button1; button2=button2; button3=button3; button4=button4; button5=button5; button6=dpi; button7=right; button8=left; button9=up; scrollup=scrollup; scrolldown=scrolldown; layout=qwerty)"
