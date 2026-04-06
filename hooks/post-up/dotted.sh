1574cf929dd7977a4b729e9a3421321900dcc4fa
Alexander Courtis <alex@courtis.org>
2026-04-07 09:28:48 +1000

    rcm: ensure postup directories exist

--- hooks/post-up/dotted.sh
+++ hooks/post-up/dotted.sh
@@ -2,6 +2,8 @@
 
 # rcup just cannot handle dotted files
 
+mkdir -p "${HOME}/.local/share/nvim/site/pack/packer/start"
+
 ln -fs \
 	"${HOME}/.dotfiles/local/share/nvim/site/pack/packer/start/.luarc.json" \
 	"${HOME}/.local/share/nvim/site/pack/packer/start/.luarc.json"
