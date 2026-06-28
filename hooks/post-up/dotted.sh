52b37ae75de446c590f0ff1f9f4866e22af16a48
Alexander Courtis <alex@courtis.org>
2026-06-28 11:42:03 +1000

    vim: remove packer luarc

--- hooks/post-up/dotted.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# rcup just cannot handle dotted files
-
-mkdir -p "${HOME}/.local/share/nvim/site/pack/packer/start"
-
-ln -fs \
-	"${HOME}/.dotfiles/local/share/nvim/site/pack/packer/start/.luarc.json" \
-	"${HOME}/.local/share/nvim/site/pack/packer/start/.luarc.json"
