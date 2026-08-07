4c1334bcd121a1810a9058963d7c467ba5b717c9
Alexander Courtis <alex@courtis.org>
2026-08-07 17:47:13 +1000

    wd-vg.sh: remove debug

--- bin/wd-vg.sh
+++ bin/wd-vg.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-export WAYLAND_DEBUG=1
+# export WAYLAND_DEBUG=1
 
 valgrind \
 	--leak-check=full \
 	--show-leak-kinds=all \
 	--suppressions="${HOME}/.vg.way-displays.supp" \
 	way-displays \
-	-L debug \
+	-L info \
 	> "${HOME}/way-displays.${XDG_VTNR}.${USER}.$(date +%Y%m%d_%H%M%S).log" \
 	2>&1 &
