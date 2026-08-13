412a3b117c0095e00999240de3254acf51e0867b
Alexander Courtis <alex@courtis.org>
2026-08-13 13:32:43 +1000

    vim: render-markdown: mc mu

--- config/nvim/lua/amc/plugins/render-markdown.lua
+++ config/nvim/lua/amc/plugins/render-markdown.lua
@@ -1,5 +1,7 @@
 local require = require("amc.require").or_nil
 
+local K = require("amc.util").K
+
 local render_markdown = require("render-markdown")
 
 if not render_markdown then
@@ -8,6 +10,8 @@ end
 
 local function on_attach()
   vim.treesitter.start()
+  K.n_lb("mc", ':s/- \\[ \\]/- [x]/g<CR>', 0, "Markdown: check box")
+  K.n_lb("mu", ':s/- \\[x\\]/- [ ]/g<CR>', 0, "Markdown: uncheck box")
 end
 
 vim.cmd("highlight! link RenderMarkdownCodeInline RenderMarkdownCode")
